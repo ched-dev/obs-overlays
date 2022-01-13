d845053960c2edd6ccb106a744d33003b5c001b1
ched-dev <cheddevdev@gmail.com>
2022-01-12 21:11:33 -0700

    TwitchPS working with events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const env = require('./env');
-const middlewares = require('./middlewares');
 const app = require('./app');
-const { ClientCredentialsAuthProvider, StaticAuthProvider } = require('@twurple/auth');
+const { ClientCredentialsAuthProvider, StaticAuthProvider, getTokenInfo } = require('@twurple/auth');
 const { ApiClient } = require('@twurple/api');
-const { EventSubMiddleware, EventSubListener } = require('@twurple/eventsub');
-const { NgrokAdapter } = require('@twurple/eventsub-ngrok')
+const { ChatClient } = require('@twurple/chat');
 
 const http = require('http');
 const server = http.createServer(app);
@@ -12,74 +10,46 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// ERROR: listener.listen() hangs
-console.log("src/index.js: Twurple Redemption Events");
+// Creates an App Token
+// const authProvider = new ClientCredentialsAuthProvider(env.client_id, env.client_secret);
 
-const authProvider = new ClientCredentialsAuthProvider(env.client_id, env.client_secret);
+// Connects with generated accessToken
+const authProvider = new StaticAuthProvider(env.client_id, env.twitch_oauth_token);
 
-// const authProvider = new StaticAuthProvider(env.client_id, env.access_token);
-const apiClient = new ApiClient({ authProvider });
+// Helix API
+// const apiClient = new ApiClient({ authProvider });
 
-
+// Chat API
+const chatClient = new ChatClient({ channels: ['ched_dev'] });
 
 (async () => {
   const port = process.env.PORT || 5000;
 
-  // const tokenInfo = await authProvider.getAccessToken();
-  // console.log("token", token)
-  // const appAuthProvider = new StaticAuthProvider(env.client_id, env.access_token, undefined, "app");
-  // const apiClient = new ApiClient({ authProvider: appAuthProvider });
+  await chatClient.connect();
+  console.log("chatClient connected");
 
-  const listener = new EventSubListener({
-    apiClient,
-    adapter: new NgrokAdapter(),
-    secret: env.secret
-  });
-
-  const user = await apiClient.users.getUserByName("ched_dev");
-  // 261129104
-  console.log("user", user.id);
-
-  const lner = listener.listen()
-  console.log("listener", lner);
-  // listener Promise { <pending> }
-  lner.then(x => {
-    console.log(`NGROK Listening: http://localhost:8000`, x);
-  }).catch(x => {
-    console.log("NGROK catch", x)
+  chatClient.onAnyMessage((message) => {
+    console.log("Message", message)
   })
 
-  // documentation way, doesn't work
-  // await listener.listen();
-  // console.log(`NGROK Listening: http://localhost:8000`);
+  const tokenInfo = await getTokenInfo(env.access_token, env.client_id)
+  console.log("tokenInfo", {
+    scopes: tokenInfo.scopes,
+    userName: tokenInfo.userName,
+    userId: tokenInfo.userId
+  })
 
-  // https://gist.github.com/OdatNurd/b9f450cb053d2138187596ddf2b554c5
-  // what I did was run that, and then go to http://127.0.0.1:4040/inspect/http in browser
+  // const user = await apiClient.users.getUserByName("ched_dev");
+  // 261129104
+  // console.log("user", user.id);
 
 
   io.on('connection', (socket) => {
     console.log('a user connected');
   });
 
-  // try {
-    // await listener.subscribeToChannelFollowEvents(user, (data) => {
-    //   console.log("Follow Event", data)
-    //   socket.emit("follow", data)
-    // })
-    // await middleware.subscribeToChannelFollowEvents(env.twitch_user_id, event => {
-    //   console.log(`${event.userDisplayName} just followed ${event.broadcasterDisplayName}!`);
-    // });
-  // } catch(e) {
-  //   console.log("Error", e)
-  // }
-
   // express server
-  app.listen(port, async () => {
-    await listener.subscribeToChannelRedemptionAddEvents(user, (data) => {
-      console.log("Redemption Event", data)
-      socket.emit("follow", data)
-    })
-    
+  app.listen(port, async () => {    
     /* eslint-disable no-console */
     console.log(`Listening: http://localhost:${port}`, /*app._router.stack*/);
     /* eslint-enable no-console */
